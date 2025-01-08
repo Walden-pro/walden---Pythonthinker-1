b4d192454118fa6df5036637cbf5860cec563aca
Walden-pro <waldenchho@gmail.com>
2025-01-08 08:59:44 +0000

    Jan 8, 2025, 8:59 AM

--- CT06_01/lesson1.py
+++ CT06_01/lesson1.py
@@ -1,10 +1,6 @@
-print(type("2014"))
-print("skibidi")
-print("8")
-print("January")
-print("2025")
+
 
 
 
 print("Hello World")
-print("My favourite food is gyat")
+print("My favourite food is gos")
