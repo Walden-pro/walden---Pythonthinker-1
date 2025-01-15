568af1905e1e3be0394c56983b86fb4b11e956c1
Walden-pro <waldenchho@gmail.com>
2025-01-15 08:46:26 +0000

    Jan 15, 2025, 8:46 AM

--- CT06_02/Untitled-1.py
+++ CT06_02/Untitled-1.py
@@ -11,7 +11,6 @@
 # 1. If the user has entered the right secret phrase, congratulate
 # them.
 input("what is the password")
-if input == 68:
-    print("correct")
+if input == 68: print("correct")
 else:
     print("wrong")
