05d68811eab7fefcd629d705c6fd02bcbfa838cf
Walden-pro <waldenchho@gmail.com>
2025-01-15 08:22:56 +0000

    Jan 15, 2025, 8:22 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,6 +1,14 @@
+# Task: Write down the steps on how to solve the problem below
 
-input(int("What is your age?"))
+# Design pseudocode for a recycling robot that sorts items
+# into bins for glass, plastic, and paper. The robot should
+# check each item's material and place it in the correct bin.
+
+
+input("What is your age?")
 if input == 18:
     print("Access denied!")
 else:
     print("Welcome!")
+
+
