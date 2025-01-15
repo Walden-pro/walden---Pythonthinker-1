6824ccfd5a3fe611a08c5f395173c382b03d44c1
Walden-pro <waldenchho@gmail.com>
2025-01-15 08:52:12 +0000

    Jan 15, 2025, 8:52 AM

--- CT06_02/Untitled-1.py
+++ CT06_02/Untitled-1.py
@@ -3,3 +3,18 @@ if input == 68:
     print("correct")
 else:
     print("wrong")
+
+
+    input("What is the password?")
+if input == 18:
+    print("Welcome!")
+else:
+    print("Access denied!")
+
+
+#     Task: Write a pseudocode that calculates the final score of a student
+# based on the following weightage:
+# _each test is scored upon 100_
+# Test 1 = 20%
+# Test 2 = 40%
+# Test 3 = 40%
