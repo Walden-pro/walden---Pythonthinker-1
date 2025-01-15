488d08f3715a4b8afdfdece0f54c75c2b9ab0305
Walden-pro <waldenchho@gmail.com>
2025-01-15 07:42:11 +0000

    Jan 15, 2025, 7:42 AM

--- CT06_01/lesson1.py
+++ CT06_01/lesson1.py
@@ -1,4 +1,4 @@
 print("My birthday is 9 march 2014")
 print("Hello world")
 print("My favourite food is mcdannies")
-print("There is a suspicious skibidi tiolet here.")
+print("There is a suspicious skibidi toilet here.")
