9c2441fa4d0fbf9e454d34f41755b1b86998bdf9
Walden-pro <waldenchho@gmail.com>
2025-01-15 08:51:51 +0000

    Jan 15, 2025, 8:51 AM

--- CT06_02/Untitled-1.py
+++ CT06_02/Untitled-1.py
@@ -1,15 +1,3 @@
-
-
-
-
-
-#Task: Using pseudocode, design a program that will check if the
-# user has entered the right secret phrase.
-
-# 1. If the user has entered a wrong secret phrase, send them an
-# error message.
-# 1. If the user has entered the right secret phrase, congratulate
-# them.
 input("what is the password")
 if input == 68:
     print("correct")
