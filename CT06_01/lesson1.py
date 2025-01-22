fba595d219a0c4d05f5bbaf1ba55045f3d066bd4
Walden-pro <waldenchho@gmail.com>
2025-01-22 08:00:10 +0000

    Jan 22, 2025, 8:00 AM

--- CT06_01/lesson1.py
+++ CT06_01/lesson1.py
@@ -1,17 +1,13 @@
-# Task: Write down the steps on how to solve the problem below
+input("what is the password")
+if input == 68:
+    print("correct")
+else:
+    print("wrong")
 
-# Design pseudocode for a recycling robot that sorts items
-# into bins for glass, plastic, and paper. The robot should
-# check each item's material and place it in the correct bin.
 
-
-#if material = rubbish
-#then put into rubbish bin incinerator
-else
-if material = glass
-then put into glass bin
- else
-if material = plastic
-then put into plastic bin
-read Warning: == 
+    input("What is the password?")
+if input == 18:
+    print("Welcome!")
+else:
+    print("Access denied!")
 
--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,22 @@
-99
+## Recap 1: Pseudo Code Practice
+# Write a pseudocode that calculates the average score of the 4
+# students:
+
+# Adam: 60
+# Brandon: 58
+# Charlie: 98
+# Daniel: 79
+
+total: 60 + 58 + 98 + 79 = 295
+divide:73.75
+
+# 1. Start
+# 2. Initialize total_score to 0
+# 3. Add Adam's score to total_score (total_score = total_score + 60)
+# 4. Add Brandon's score to total_score (total_score = total_score + 58)
+# 5. Add Charlie's score to total_score (total_score = total_score + 98)
+# 6. Add Daniel's score to total_score (total_score = total_score + 79)
+# 7. Calculate the number of students (num_students = 4)
+# 8. Calculate the average score (average_score = total_score / num_students)
+# 9. Print the average_score
+# 10. End
