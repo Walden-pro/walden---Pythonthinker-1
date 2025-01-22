b348f56f00093bac6b448a86b1a9a3d7d68a765a
Walden-pro <waldenchho@gmail.com>
2025-01-22 08:04:32 +0000

    Jan 22, 2025, 8:04 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,15 +1,12 @@
 ## Recap 1: Pseudo Code Practice
 # Write a pseudocode that calculates the average score of the 4
 # students:
-
 # Adam: 60
 # Brandon: 58
 # Charlie: 98
 # Daniel: 79
-
 # total: 60 + 58 + 98 + 79 = 295
 # divide:73.75
-
 # 1. Start
 # 2. Initialize total_score to 0
 # 3. Add Adam's score to total_score (total_score = total_score + 60)
