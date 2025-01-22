01180fad4f6f781f6166af0a1ca2b3586ce60b80
Walden-pro <waldenchho@gmail.com>
2025-01-22 08:46:47 +0000

    Jan 22, 2025, 8:46 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -38,9 +38,9 @@
 #num1 = 19978
 #num2 = 456
 
-print(num1 - num2)
-print(num1 * num2)
-print(num1 + num2)
+#print(num1 - num2)
+#print(num1 * num2)
+p#rint(num1 + num2)
 print(num1 / num2)
 
 print( 2 + 4 * 5 / 6 + 3 - 6 / 2)
