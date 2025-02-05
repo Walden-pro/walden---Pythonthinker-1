e32df05680bc65dae0c0f5783d8c53662d0246bd
Walden-pro <waldenchho@gmail.com>
2025-02-05 07:36:01 +0000

    Feb 5, 2025, 7:36 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -17,8 +17,6 @@ print("3.Multiply")
 print("4.Divide")
 while True:
     choice = input("Enter choice(1/2/3/4): ")
-
-
     if choice in ('1', '2', '3', '4'):
         try:
             num1 = float(input("Enter first number: "))
@@ -26,21 +24,15 @@ while True:
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
-
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
-
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
-
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
-
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
         
-
-
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
           break
