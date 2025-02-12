230f097e49d948c55b7f3399ffe1001f528c04cf
Walden-pro <waldenchho@gmail.com>
2025-02-12 08:43:06 +0000

    Feb 12, 2025, 8:43 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -70,13 +70,11 @@ for count in range(11, 1000, 12):
     print(count)
 
     
-# **Task 5a**:
-# Print numbers from 1 to 5 using a 'for' loop.
-for count in range(1, 1000, 12):
-    print(count)
+# **Task 6a**:
+# Use a 'for' loop to print numbers from 2 to 24 in multiples of 2.
 
-# **Task 5b**:
-# Print numbers from 51 to 100 using a 'for' loop.
+# **Task 6b**:
+# Use a 'for' loop to print numbers from 8 to 96 in multiples of 8.
 
-# **Task 5c**:
-# Print numbers from 18 to 29 using a 'for' loop.
+# **Task 6c**:
+# Use a 'for' loop to print numbers from 5 to 1 in descending order.
