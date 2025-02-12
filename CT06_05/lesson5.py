e80621c88a48ea92f88488627789988acb46123b
Walden-pro <waldenchho@gmail.com>
2025-02-12 07:55:18 +0000

    Feb 12, 2025, 7:55 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -16,7 +16,7 @@ name = input("what is the birthday person name?")
 age = input("what age are you")
 
 
-print("happy " + age + "th birthday, " + name + "!")
+print("Happy " + age + "th birthday, " + name + "!")
 # Finally, the program prints out a personalized invitation
 # message.
 
