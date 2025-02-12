9ec53c1e29d3c568fe8020ed1313b6f3c237bde5
Walden-pro <waldenchho@gmail.com>
2025-02-12 07:46:39 +0000

    Feb 12, 2025, 7:46 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -10,7 +10,7 @@
 # 1. birthday person's name
 # 2. the age that they are turning that year
 # 3. personal message from the sender
-input()
+input("what is the birthday person name?")
 # Finally, the program prints out a personalized invitation
 # message.
 
