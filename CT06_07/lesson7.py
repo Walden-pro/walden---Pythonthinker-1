c0757a3948dbb11ce973b40bac8aefc7a70ff7b9
Walden-pro <waldenchho@gmail.com>
2025-02-23 04:20:48 +0000

    Feb 23, 2025, 4:20 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,5 +1,36 @@
 print("Hello from lesson 7")
 
+
+
+
+
+
+# Lesson 7 - For Loop (Part 2)
+
+## Recap 1: Debugging Average Score Program
+
+### There is a total of 3 bugs in the following program.
+### Identify and fix the bugs:
+
+score_one = 80
+score_two = 90
+score_three = 75
+
+total = score_one + score_two  + score_three
+
+average_score = total / 3
+
+student_name = "Alex"
+
+print("Average score for " + student_name + " is: " + average_score)
+
+
+
+
+
 ## Task 1: For Loop: 1 to 10 using range(start, stop)
 
-Use a 'for' loop to print numbers from 1 to 10.
+#Use a 'for' loop to print numbers from 1 to 10.
+
+for i in range(1, 10 + 1, 1):
+    print(i)
