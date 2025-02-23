8637e859e24c982cec9c05c79d38dd321b79b55c
Walden-pro <waldenchho@gmail.com>
2025-02-23 03:34:58 +0000

    Feb 23, 2025, 3:34 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,17 +1,1 @@
 print("Hello from lesson 6")
-
-
-start = int(input("Starting number "))
-stop = int(input("Stopping number "))
-if start > stop:
-    for count in range(start, stop -1 , -1 ):
-       print(count)
-else:
-    for count in range(start, stop+1):
-        print(count)
-
-
-
-
-for i in range(10, 1 - 1, -1):
-    print(i)
