39bb64952f961c47bd33fe5cd2f1eb725c46b5dd
Walden-pro <waldenchho@gmail.com>
2025-02-23 04:31:55 +0000

    Feb 23, 2025, 4:31 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -69,5 +69,5 @@ print(input("What word would you like to repeat?"))
 word = input
 print(input("How many times would you like to repeat?"))
 times = input
-total = word 
+total = word * times
 print(word * times)
