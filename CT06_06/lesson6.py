be613c9cb7ebeb9079c7232ee3209748f942a5b5
Walden-pro <waldenchho@gmail.com>
2025-02-23 03:53:36 +0000

    Feb 23, 2025, 3:53 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,106 @@
-print("Hello from lesson 6")
+
+
+
+
+
+## Task 1: Syntax Errors
+#Fix the errors in the following:
+
+##**Task 1a**:
+# for i in range(3):
+
+#    print("Hello, World!")
+
+#**Task 1b**:
+##for i in range(5):
+#print(i)
+
+#**Task 1c**:
+#print("Hello, World!)
+
+#**Task 1d**:
+#if = 5
+
+#**Task 1e**:
+#print "Hello, World!"
+
+
+
+for i in range(3):
+    print()
+
+for i in range(5):
+    print(i)
+
+
+print("Hello, World!")
+
+
+if_ = 5
+
+
+print("Hello, World!")
+
+
+
+
+## Task 2: Name Errors
+#Fix the errors in the following:
+
+#**Task 2a**:
+#print(age)
+
+#**Task 2b**:
+#name = "Alice"
+#print(nam)
+
+#**Task 2c**:
+#print(x)
+#x = 5
+
+#**Task 2d**:
+#pint("Hello, World!")
+
+
+
+age = 1
+print(age)
+
+
+name = "Alice"
+print(name)
+
+print(x)
+x = 5
+
+print("Hello, World!")
+
+
+
+
+## Task 3: Type Errors
+#Fix the errors in the following:
+
+#**Task 3a**:
+#age = "25"
+##print(age + 1)
+
+#**Task 3b**:
+#number = 10
+#print(number - "5")
+
+#**Task 3c**:
+#print("Repeat" * "3")
+
+#**Task 3d**:
+#year = 2023
+#print("The year is " + year)
+
+#**Task 3e**:
+#x = "10"
+#y = x / 2
+
+#**Task 3f**:
+#end = "5"
+#for i in range(end):
+#    print(i)
