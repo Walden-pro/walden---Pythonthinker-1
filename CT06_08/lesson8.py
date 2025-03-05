8ad7bda5657a0cb0849d48566c73c29f1584f81c
Walden-pro <waldenchho@gmail.com>
2025-03-05 08:09:15 +0000

    Mar 5, 2025, 8:09 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -6,11 +6,11 @@
 
 
 
-start = int(input("Start"))
+start = int(input("Start "))
 # This line stores the input into start variable
-end = int(input("End"))
+end = int(input("End "))
 # This line of code stores the input into the end variable
-increment = int(input("Increment"))
+increment = int(input("Increment "))
 # This line of code stores the input into a increment variable
 for i in range(start, end, increment):
 # the variables are an integer, i variable is storing the numbers and calculating, start to end increasing by increment
