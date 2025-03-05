bafede3861f950b09c0b6400a79bb0f95df31e2e
Walden-pro <waldenchho@gmail.com>
2025-03-05 08:06:41 +0000

    Mar 5, 2025, 8:06 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -13,5 +13,5 @@ end = int(input("End"))
 increment = int(input("Increment"))
 # This line of code stores the input into a increment variable
 for i in range(start, end, increment):
-# the variables are an integer, 
+# the variables are an integer, i variable is storing the 
     print(i)
