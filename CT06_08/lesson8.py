14ddaeeb8bab1a923f08f63a05d50c90b12097a1
Walden-pro <waldenchho@gmail.com>
2025-03-05 07:55:45 +0000

    Mar 5, 2025, 7:55 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,4 +1,4 @@
 name = input("What is your name? ")
 # That is asking for the name and puts it into the variable name.
 print("Nice to meet you, "  + name )
-# This line prints out the 'nice to meet you 
+# This line prints out the 'nice to meet you + the variable 'name'
