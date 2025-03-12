4930709c8cc4e0839883714ebe7a8e6cc7b6000e
Walden-pro <waldenchho@gmail.com>
2025-03-12 07:41:23 +0000

    Mar 12, 2025, 7:41 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -11,7 +11,7 @@
 
 Product = 0
 for i in range(1 , 6):
-    Product = sum * int(input("What is number " + str(i) + " ? "))
+    Product = Product * int(input("What is number " + str(i) + " ? "))
 
 print("Product of the 5 numbers is " , sum)
 
