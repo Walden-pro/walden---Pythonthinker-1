0e81e693f766bcc349433ef265aebf24434e5b75
Walden-pro <waldenchho@gmail.com>
2025-03-12 07:37:00 +0000

    Mar 12, 2025, 7:37 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -8,8 +8,17 @@
 # 1. Using a for loop, ask the user for 5 numbers one at a time.
 # 2. Calculate the multiplication for these 5 numbers and print it out.
 
+## Task 1: 'time' library
 
+# **Task 1a**:
+# Import the 'time' library and make use of the 'time.sleep()'
+# function to create a 10 seconds countdown timer that counts
+# to 1, printing the number of seconds remaining every second.
 
+# **Task 1b**:
+# Modify your code from Task 1a to include an 'input()' function
+# asking the user for the number to countdown from, before
+# counting down every second from the number given by the user.
 
 
 
