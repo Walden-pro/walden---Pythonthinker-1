6ceee86d3c61a9504466e4707fca49903022ab6e
Walden-pro <waldenchho@gmail.com>
2025-03-12 08:39:27 +0000

    Mar 12, 2025, 8:39 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -127,19 +127,6 @@ ans = int(input(str(num1) + "+" + str(num2) + "=" ))
 print(ansc == ans)
   
 
-
-# **Task 4a**: Math Question Generator
-# Using the 'random' library, generate 2 numbers between 1 and 50
-# that the user must add together.
-
-# The output should be one of the following:
-# 1. True (If the answer is correct)
-# 2. False (If the answer is wrong)
-
-# Example:
-# What is 2 + 5? << 7 >>
-# True
-
 # **Task 4b**: Range Guesser
 # Create a program that generates a random number between 1 and
 # 50.
