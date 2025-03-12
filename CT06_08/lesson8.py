296cc36d33aa63a2642809d9c63368b86a3b0094
Walden-pro <waldenchho@gmail.com>
2025-03-12 08:38:47 +0000

    Mar 12, 2025, 8:38 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -123,7 +123,7 @@ import random
 num1 = random.randint(1,50)
 num2 = random.randint(1,50)
 ansc = num1 + num2
-ans = input(str(num1) + "+" + str(num2) + "=" )
+ans = int(input(str(num1) + "+" + str(num2) + "=" ))
 print(ansc == ans)
   
 
