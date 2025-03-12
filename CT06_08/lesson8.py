e2383d3925ba082d1e4345c2ec309036d59d9853
Walden-pro <waldenchho@gmail.com>
2025-03-12 07:34:32 +0000

    Mar 12, 2025, 7:34 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -11,5 +11,4 @@
 # numbers.
 
 # 1. Using a for loop, ask the user for 5 numbers one at a time.
-# 2. Calculate the multiplication for these 5 numbers and print
-#    it out.
+# 2. Calculate the multiplication for these 5 numbers and print it out.
