c67b60dc27d3cce4c7b820c217c4250dc12d11ad
Walden-pro <waldenchho@gmail.com>
2025-03-12 07:32:47 +0000

    Mar 12, 2025, 7:32 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,18 +1,18 @@
-# name = input("What is your name? ")
-# That is asking for the name and puts it into the variable 'name'
-# print("Nice to meet you, "  + name )
-# This line prints out the 'nice to meet you + the variable 'name'
+# # name = input("What is your name? ")
+# # That is asking for the name and puts it into the variable 'name'
+# # print("Nice to meet you, "  + name )
+# # This line prints out the 'nice to meet you + the variable 'name'
 
 
 
 
-start = int(input("Start: "))
-# This line stores the input into start variable
-end = int(input("End: "))
-# This line of code stores the input into the end variable
-increment = int(input("Increment: "))
-# This line of code stores the input into a increment variable
-for i in range(start, end, increment):
-# the variables are an integer, i variable is storing the numbers and calculating, start to end increasing by increment
-    print(i)
-# This line prints the code by the numbers.
+# start = int(input("Start: "))
+# # This line stores the input into start variable
+# end = int(input("End: "))
+# # This line of code stores the input into the end variable
+# increment = int(input("Increment: "))
+# # This line of code stores the input into a increment variable
+# for i in range(start, end, increment):
+# # the variables are an integer, i variable is storing the numbers and calculating, start to end increasing by increment
+#     print(i)
+# # This line prints the code by the numbers.
