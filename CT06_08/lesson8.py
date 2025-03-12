67f95f8895d25c90b218343775a6a2b2c25bcdf3
Walden-pro <waldenchho@gmail.com>
2025-03-12 07:34:17 +0000

    Mar 12, 2025, 7:34 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,18 +1,15 @@
-# # name = input("What is your name? ")
-# # That is asking for the name and puts it into the variable 'name'
-# # print("Nice to meet you, "  + name )
-# # This line prints out the 'nice to meet you + the variable 'name'
 
 
 
 
-# start = int(input("Start: "))
-# # This line stores the input into start variable
-# end = int(input("End: "))
-# # This line of code stores the input into the end variable
-# increment = int(input("Increment: "))
-# # This line of code stores the input into a increment variable
-# for i in range(start, end, increment):
-# # the variables are an integer, i variable is storing the numbers and calculating, start to end increasing by increment
-#     print(i)
-# # This line prints the code by the numbers.
+
+# Lesson 8 - Importing Libraries, Boolean & Conditions
+
+## Recap 1: Product of 5 numbers
+
+# Write a program to calculate the product (multiplication) of 5
+# numbers.
+
+# 1. Using a for loop, ask the user for 5 numbers one at a time.
+# 2. Calculate the multiplication for these 5 numbers and print
+#    it out.
