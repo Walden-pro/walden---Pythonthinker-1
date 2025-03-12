4337ccd1df95b2ed19e64a9fbb0cc96be22c61d8
Walden-pro <waldenchho@gmail.com>
2025-03-12 07:39:54 +0000

    Mar 12, 2025, 7:39 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -13,7 +13,7 @@ sum = 0
 for i in range(1 , 6):
     sum = sum * int(input("What is number " + str(i) + " ? "))
 
-print("Sum of the 5 numbers is " , sum)
+print("Product of the 5 numbers is " , sum)
 
 
 
