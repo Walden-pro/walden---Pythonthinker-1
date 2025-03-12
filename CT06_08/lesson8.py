a5e4d372c5bde257317464989ca23d04ab012919
Walden-pro <waldenchho@gmail.com>
2025-03-12 07:44:19 +0000

    Mar 12, 2025, 7:44 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -9,7 +9,7 @@
 # 2. Calculate the multiplication for these 5 numbers and print it out.
 
 
-Product = 1
+Product = 1 # accumulator variable
 for i in range(1 , 6):
     Product = Product * int(input("What is number " + str(i) + " ? "))
 
