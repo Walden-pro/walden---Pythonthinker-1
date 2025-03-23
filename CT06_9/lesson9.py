11f0f24079bf8b01ab6d1ad8e9aa315a03a883e1
Walden-pro <waldenchho@gmail.com>
2025-03-23 03:54:53 +0000

    Mar 23, 2025, 3:54 AM

--- CT06_9/lesson9.py
+++ CT06_9/lesson9.py
@@ -48,4 +48,5 @@ print("All numbers are even or odd " ,all_even_odd)
 # 1. Ask the user to input the number of days a book has been borrowed
 # 2. If the book has been borrowed for more than 25 days:
 #     print "Remember to return your book!"
+
 days_borrowed = int(input("number of days the book has been borrowed"))
