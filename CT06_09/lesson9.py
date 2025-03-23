3a17044611907764875f3056c0f0d8a3844b7501
Walden-pro <waldenchho@gmail.com>
2025-03-23 03:25:10 +0000

    Mar 23, 2025, 3:25 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -27,3 +27,7 @@ import random
 num1 = random.randint(1, 6)
 num2 = random.randint(1, 6)
 num3 = random.randint(1, 6)
+
+print("1st number:", num1)
+print("2nd number:", num2)
+print("3rd number:", num3)
