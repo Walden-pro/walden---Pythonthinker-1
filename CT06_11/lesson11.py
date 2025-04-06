4d38785ff5b350ae3561b64691a5cc5dd605dd1c
Walden-pro <waldenchho@gmail.com>
2025-04-06 04:07:46 +0000

    Apr 6, 2025, 4:07 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -170,4 +170,5 @@ burger = input("Do you want a burger")
 fries = input("Do you want a drink")
 drink = input("Do you want some fries")
 
-if burger == "yes" and fries == "yes" and drink == "no":
+if burger == "yes" and fries == "yes" and not drink == "yes":
+    print()
