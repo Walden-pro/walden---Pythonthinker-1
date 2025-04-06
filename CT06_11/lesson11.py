ccdb4f37049f536113a87b183307ebc5ff04a6d8
Walden-pro <waldenchho@gmail.com>
2025-04-06 03:15:08 +0000

    Apr 6, 2025, 3:15 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -10,15 +10,15 @@
 #     px <= 500: "Where are you getting this money from?!"
 #     px > 500: "Don't even think about it!"
  
-px = int(input("The price of the item: $"))
-if px <= 5:
-        print("Sounds good!")
-elif px <= 50:
-    print("Are you sure you need this?")
-elif px <= 500:
-    print("Where are you getting this money from?!")
-else:
-    print("Don't even think about it!")
+# px = int(input("The price of the item: $"))
+# if px <= 5:
+#         print("Sounds good!")
+# elif px <= 50:
+#     print("Are you sure you need this?")
+# elif px <= 500:
+#     print("Where are you getting this money from?!")
+# else:
+#     print("Don't even think about it!")
 
 
 
