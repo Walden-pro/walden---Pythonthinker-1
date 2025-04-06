0537e598dfd98d1f5337c049909c90ac3bcfe837
Walden-pro <waldenchho@gmail.com>
2025-04-06 04:25:07 +0000

    Apr 6, 2025, 4:25 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -214,7 +214,7 @@
 # 4. Otherwise, print "Game is paused or inactive."
 
 
-game_status = "active"
+game_status = "paused"
 if game_status == "active" or not game_status == "paused":
     print("Game in progress")
 else:
