baeb7de471995af04b0c50e5c3c57accc73c50f6
Walden-pro <waldenchho@gmail.com>
2025-04-06 03:12:04 +0000

    Apr 6, 2025, 3:12 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -16,4 +16,6 @@ if px <= 5:
 elif px <= 50:
     print("Are you sure you need this?")
 elif px <= 500:
-    print("")
+    print("Where are you getting this money from?!")
+else:
+    print("Don't even think about it!")
