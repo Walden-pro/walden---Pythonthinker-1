a140959a5f6684bb0497433f90f630965e28c0a3
Walden-pro <waldenchho@gmail.com>
2025-04-06 04:09:00 +0000

    Apr 6, 2025, 4:09 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -166,9 +166,9 @@
 #     print "Won't you get thirsty?"
 
 
-burger = input("Do you want a burger")
-fries = input("Do you want a drink")
-drink = input("Do you want some fries")
+burger = input("Do you want a burger ")
+fries = input("Do you want a drink ")
+drink = input("Do you want some fries ")
 
 if burger == "yes" and fries == "yes" and not drink == "yes":
     print("Won't you get thirsty?")
