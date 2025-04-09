1ab8ee151c09e62eff2757e742b609a58ad85ab2
Walden-pro <waldenchho@gmail.com>
2025-04-09 07:53:39 +0000

    Apr 9, 2025, 7:53 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -74,6 +74,7 @@
 # modify your answer for Task 1a to terminate the 'while' loop when
 # number of visitors is 30.
 
-
-visi
-
+visitors = 0
+if visitors == 30:
+    print("The restaurant is at max capacity!")
+    break
