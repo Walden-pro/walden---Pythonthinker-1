dd0cebd97c0ad0e472057d7746162eed9da540d6
Walden-pro <waldenchho@gmail.com>
2025-04-09 07:40:01 +0000

    Apr 9, 2025, 7:40 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -16,3 +16,4 @@ bruh = int(input("What is the number?"))
 if bruh % 3 == 0 and bruh % 5 == 0:
     print("The number is divisible by 3 and 5!")
 else:
+    print("The number is not divisible by 3 and 5!")
